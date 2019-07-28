01e1fcbce0afa2e1951934461c33b291c616f860
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-28 17:27:21 +0400

    Add navbar

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './Nav.css'
+
+function Nav(){
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Nav
