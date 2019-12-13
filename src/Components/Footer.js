c3c356eb421023864c1abf9d1fa1277a6c0786f6
Hemlatab <hemlata.b@hotmail.co.uk>
2019-12-13 15:47:01 +0400

    Add footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
