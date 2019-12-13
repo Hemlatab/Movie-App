317b24df91dea3efede8aa5a5ed9b3186943f6bd
Hemlatab <hemlata.b@hotmail.co.uk>
2019-12-13 16:28:17 +0400

    Add Fontawesome icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <FontAwesomeIcon icon="fa-film" />
       <h1>Movie App</h1>
     </div>
   );
