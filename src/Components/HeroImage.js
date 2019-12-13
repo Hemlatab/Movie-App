9ba4a27e2b856601183f99eb6608bea9a752fd6f
Hemlatab <hemlata.b@hotmail.co.uk>
2019-12-13 16:45:48 +0400

    Add components

---
+++ src/Components/HeroImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function HeroImage() {
+  return (
+    <div>
+      <h1>Hero Image</h1>
+    </div>
+  );
+}
+
+export default HeroImage;
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
