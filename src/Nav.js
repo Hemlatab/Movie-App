3b5bfe9a652ef2e0de25af572f9e917c3a48af98
Hemlatab <hemlata.b@hotmail.co.uk>
2019-12-13 15:47:20 +0400

    Add style

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
 
-function Nav(){
-    return(
-        <div>
-            <ul>
-                <li>Home</li>
-            </ul>
-        </div>
-    )
+function Nav() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
 }
 
-
-export default Nav
+export default Nav;
